4a1fbbbd782223f2ea3d76c73ffdea1ca451dccb
Maniharika-1 <maniharika1998@gmail.com>
2022-11-21 18:43:17 +0530

    Added NetModule class to provide Retrofit and API Service dependencies.

---
+++ app/src/main/java/com/example/newsapiclient/presentation/di/NetModule.kt
@@ -0,0 +1,28 @@
+package com.example.newsapiclient.presentation.di
+
+import com.example.newsapiclient.BuildConfig
+import com.example.newsapiclient.data.api.NewsAPIService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit() : Retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideNewsAPIService(retrofit: Retrofit) : NewsAPIService = retrofit.create(NewsAPIService::class.java)
+
+}
