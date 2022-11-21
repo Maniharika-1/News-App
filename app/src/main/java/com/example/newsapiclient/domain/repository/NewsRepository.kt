a5be7a1653f954209527cf295d5b15771497405e
Maniharika-1 <maniharika1998@gmail.com>
2022-11-21 17:23:28 +0530

    Added data layer news repository interface.

---
+++ app/src/main/java/com/example/newsapiclient/domain/repository/NewsRepository.kt
@@ -0,0 +1,19 @@
+package com.example.newsapiclient.domain.repository
+
+import com.example.newsapiclient.data.model.APIResponse
+import com.example.newsapiclient.data.model.Article
+import com.example.newsapiclient.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+
+    //network calls
+    suspend fun getNewsHeadlines(country: String, page: Int) : Resource<APIResponse>
+    suspend fun getSearchedNews(country: String, searchQuery: String, page: Int): Resource<APIResponse>
+
+    //local database calls
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews() : Flow<List<Article>>
+
+}
