477e936a224fdaa3d0db66f6ab6d6a8203ee11c3
Maniharika-1 <maniharika1998@gmail.com>
2022-11-21 17:27:37 +0530

    Added Resource sealed class which can be used to show the status of API response - success, loading or error.

---
+++ app/src/main/java/com/example/newsapiclient/data/util/Resource.kt
@@ -0,0 +1,11 @@
+package com.example.newsapiclient.data.util
+
+//this class is used to show the correct status of APIResponse - success, loading or error
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T): Resource<T>(data)
+    class Loading<T>(data: T? = null): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+}
