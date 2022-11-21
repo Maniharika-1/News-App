5c45ddc3299bec65d2638ed5487be9a9c40d94f9
Maniharika-1 <maniharika1998@gmail.com>
2022-11-21 17:24:34 +0530

    Added use case classes.

---
+++ app/src/main/java/com/example/newsapiclient/domain/usecase/GetSearchedNewsUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.newsapiclient.domain.usecase
+
+import com.example.newsapiclient.data.model.APIResponse
+import com.example.newsapiclient.data.util.Resource
+import com.example.newsapiclient.domain.repository.NewsRepository
+
+class GetSearchedNewsUseCase(private val newsRepository: NewsRepository) {
+
+    suspend fun execute(country: String, searchQuery: String, page: Int) : Resource<APIResponse> = newsRepository.getSearchedNews(country, searchQuery, page)
+}
