ca7280346a3514036eaf03b85a8d1222b15a604f
Maniharika-1 <maniharika1998@gmail.com>
2022-11-21 16:02:05 +0530

    Added model classes.

---
+++ app/src/main/java/com/example/newsapiclient/data/model/Source.kt
@@ -0,0 +1,12 @@
+package com.example.newsapiclient.data.model
+
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Source(
+    @SerializedName("id")
+    val id: Any?,
+    @SerializedName("name")
+    val name: String?
+):Serializable
