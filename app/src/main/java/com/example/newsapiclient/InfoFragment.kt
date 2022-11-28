85e4a6fc37ce605e5b736f5ba2f23d06b93c1ece
Maniharika-1 <maniharika1998@gmail.com>
2022-11-28 15:35:36 +0530

    Added Info Fragment. Fetch article sent from NewsFragment and load it on web view.

---
+++ app/src/main/java/com/example/newsapiclient/InfoFragment.kt
@@ -0,0 +1,37 @@
+package com.example.newsapiclient
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
+import com.example.newsapiclient.databinding.FragmentInfoBinding
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class InfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentInfoBinding.bind(view)
+        val args : InfoFragmentArgs by navArgs()
+        val selectedArticle = args.selectedArticle
+        binding.newsInfoWebView.apply {
+            webViewClient = WebViewClient()
+            if(selectedArticle.url != null)
+                loadUrl(selectedArticle.url)
+        }
+    }
+}
