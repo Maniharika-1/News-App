aff741feeef4bfeb8fdf01eea2e0fd0f3630b03c
Maniharika-1 <maniharika1998@gmail.com>
2022-11-29 17:26:54 +0530

    Adding SavedFragment to show the list of saved news items.

---
+++ app/src/main/java/com/example/newsapiclient/SavedFragment.kt
@@ -0,0 +1,59 @@
+package com.example.newsapiclient
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsapiclient.databinding.FragmentSavedBinding
+import com.example.newsapiclient.presentation.adapter.NewsAdapter
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class SavedFragment : Fragment() {
+
+    private lateinit var binding: FragmentSavedBinding
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var viewModel: NewsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSavedBinding.bind(view)
+        newsAdapter = (activity as MainActivity).newsAdapter
+        viewModel = (activity as MainActivity).viewModel
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("selected_article", it)
+            }
+
+            findNavController().navigate(R.id.action_savedFragment_to_infoFragment, bundle)
+        }
+
+        initRecyclerView()
+
+        viewModel.getSavedNews().observe(viewLifecycleOwner){
+            newsAdapter.differ.submitList(it)
+        }
+    }
+
+    private fun initRecyclerView() {
+        binding.savedNewsRecyclerView.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+}
