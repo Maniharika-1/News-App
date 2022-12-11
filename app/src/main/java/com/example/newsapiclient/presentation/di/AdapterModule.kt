f65876e88dd0d8ce04d7deaac71ac19c073f0073
Maniharika-1 <maniharika1998@gmail.com>
2022-12-11 17:42:49 +0530

    Added SavedNewsAdapter provider in AdapterModule.

--- app/src/main/java/com/example/newsapiclient/presentation/di/AdapterModule.kt
+++ app/src/main/java/com/example/newsapiclient/presentation/di/AdapterModule.kt
@@ -1,6 +1,7 @@
 package com.example.newsapiclient.presentation.di
 
 import com.example.newsapiclient.presentation.adapter.NewsAdapter
+import com.example.newsapiclient.presentation.adapter.SavedNewsAdapter
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,5 +15,9 @@ class AdapterModule {
     @Singleton
     @Provides
     fun provideNewsAdapter(): NewsAdapter = NewsAdapter()
+
+    @Singleton
+    @Provides
+    fun provideSavedNewsAdapter(): SavedNewsAdapter = SavedNewsAdapter()
 
 }
