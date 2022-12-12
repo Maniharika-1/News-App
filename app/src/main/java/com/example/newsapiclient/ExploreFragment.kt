cd7c769fe02cab5c30d2b79356187b51ca599a23
Maniharika-1 <maniharika1998@gmail.com>
2022-12-12 13:46:34 +0530

    Added explore fragment in nav_graph.

---
+++ app/src/main/java/com/example/newsapiclient/ExploreFragment.kt
@@ -0,0 +1,25 @@
+package com.example.newsapiclient
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapiclient.databinding.FragmentExploreBinding
+
+class ExploreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+
+    private lateinit var binding: FragmentExploreBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentExploreBinding.bind(view)
+    }
+}
